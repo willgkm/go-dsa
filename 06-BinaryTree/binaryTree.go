342e1cb344ce7ea5bb02b1442e55a0f219a7cadf
williangk <wkuviatkovski@gmail.com>
2024-06-24 21:26:34 -0300

    criando metodo para verificar a altura da arvore

--- 06-BinaryTree/binaryTree.go
+++ 06-BinaryTree/binaryTree.go
@@ -13,19 +13,18 @@ func createNode(data int8) *Node {
 	return &Node{data: data, rightChild: nil, leftChild: nil}
 }
 
-func (tree *BinaryTree) insertAtRoot(data int8) {
+func insertAtRoot(tree *BinaryTree, data int8) {
 	newNode := createNode(data)
 	tree.root = newNode
 }
 
 func (tree *BinaryTree) insert(data int8) {
 	if tree.root == nil {
-		tree.insertAtRoot(data)
+		insertAtRoot(tree, data)
 	} else {
 
 		current := tree.root
 		for {
-
 			if current.data > data {
 				if current.leftChild == nil {
 					current.leftChild = createNode(data)
@@ -42,11 +41,35 @@ func (tree *BinaryTree) insert(data int8) {
 				}
 			}
 		}
+	}
+}
 
+//TODO: make insert() on a recursive way
+
+func (tree *BinaryTree) height() int8 {
+
+	var leftHeight, rightHeight int8
+
+	current := tree.root
+	for current.leftChild != nil {
+		leftHeight++
+		current = current.leftChild
+	}
+
+	current = tree.root
+	for current.rightChild != nil {
+		rightHeight++
+		current = current.rightChild
+	}
+
+	if leftHeight > rightHeight {
+		return leftHeight
+	} else {
+		return rightHeight
 	}
 
 }
 
-func main() {
+// TODO: make a InOrderTraversal method
 
-}
+func main() {}
--- 06-BinaryTree/binaryTree_test.go
+++ 06-BinaryTree/binaryTree_test.go
@@ -5,6 +5,20 @@ import (
 	"testing"
 )
 
+func setupTree() *BinaryTree {
+
+	tree := BinaryTree{}
+	tree.insert(4)
+	tree.insert(2)
+	tree.insert(6)
+	tree.insert(1)
+	tree.insert(3)
+	tree.insert(5)
+	tree.insert(7)
+	return &tree
+
+}
+
 func TestBinaryTree(t *testing.T) {
 
 	t.Run("should create new node in the root ", func(t *testing.T) {
@@ -19,19 +33,33 @@ func TestBinaryTree(t *testing.T) {
 
 	t.Run("should insert values in a balance way in a binary tree", func(t *testing.T) {
 
-		tree := BinaryTree{}
-		tree.insert(4)
-		tree.insert(2)
-		tree.insert(6)
-		tree.insert(1)
-		tree.insert(3)
-		tree.insert(5)
-		tree.insert(7)
-		got := tree
+		tree := setupTree()
 
+		got := tree
 		want := BinaryTree{root: &Node{data: 4,
 			leftChild:  &Node{data: 2, leftChild: &Node{data: 1}, rightChild: &Node{data: 3}},
 			rightChild: &Node{data: 6, leftChild: &Node{data: 5}, rightChild: &Node{data: 7}}}}
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("should get the height of a 'balance' tree", func(t *testing.T) {
+
+		tree := setupTree()
+
+		got := tree.height()
+		want := int8(2)
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("should get the height of a tree with a bigger right side", func(t *testing.T) {
+
+		tree := setupTree()
+		tree.insert(8)
+		tree.insert(9)
+		tree.insert(10)
+
+		got := tree.height()
+		want := int8(5)
 		assertCorrectMessage(t, got, want)
 	})
 
